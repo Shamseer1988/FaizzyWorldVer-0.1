b8ea47352a12f84f54dc7e44ae72bcaf5b4baf39
“shamseer1988” <m.shamsee1988@gmail.com>
2021-03-19 13:31:31 +0300

    Faizzy World After Fixing Responsive issues

---
+++ js/custom.js
@@ -0,0 +1,81 @@
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+// Bootstrap Model Youtube video auto stop s
+      
+$(document).ready(function() {
+
+  // Gets the video src from the data-src on each button
+  
+  var $videoSrc;  
+  
+  $('.video-btn').click(function() {
+      $videoSrc = $(this).data( "src" );
+  });
+  console.log($videoSrc);
+  
+    
+    
+  // when the modal is opened autoplay it  
+  $('#myModal').on('shown.bs.modal', function (e) {
+      
+  // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+  $("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1&mute=1"); 
+  })
+    
+    
+  // stop playing the youtube video when I close the modal
+  $('#myModal').on('hide.bs.modal', function (e) {
+      // a poor man's stop video
+      $("#video").attr('src',$videoSrc); 
+  }) 
+      
+  // document ready  
+  });
+  
+
+
+
+
+ 
